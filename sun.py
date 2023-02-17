ab1704171375cc13d7e17b39e91486f8fdfe15ab
Patrick van Staveren <trick@vanstaveren.us>
2023-02-17 22:56:23 +0000

    cleanup, schedule jobs from now onwards, for 30 days instead of 20, and do lots of angles (10/20/30/40/50). throws errors about higher angles which aren't happening this time of year, but that's ok

--- sun.py
+++ sun.py
@@ -8,31 +8,17 @@ from astropy.time import Time
 import astropy.coordinates
 import astropy.units as u
 
-home = EarthLocation(lat=41.87699*u.deg, lon=-88.0808*u.deg, height=234*u.m)
-print(home)
-utcoffset = -6*u.hour
-time = Time('2023-01-18 07:15:00') - utcoffset
-print(time)
-astropy.coordinates.get_sun(time)
-sc = astropy.coordinates.get_sun(time)
-print(sc)
-sc.transform_to(AltAz(obstime=time,location=home))
-a = sc.transform_to(AltAz(obstime=time,location=home))
-print("sun position at approx sunrise time")
-print(a)
-print(a.alt)
-print(a.az)
-
-print()
-#import astroplan
 from astroplan import Observer, FixedTarget
-o = Observer(home)
 
-start_time = Time('2023-01-31 12:00:00') - utcoffset
+home = EarthLocation(lat=41.87699*u.deg, lon=-88.0808*u.deg, height=234*u.m)
+o = Observer(home)
+utcoffset = -6*u.hour
+
+start_time = Time('2023-02-18 12:00:00') - utcoffset
 
 print("Scheduling: sun set snaps")
 time = start_time
-for x in range(0, 20):
+for x in range(0, 30):
     time += 1
     s = o.sun_set_time(time, which='next') # horizon=20
     print("ISO: {0.iso}, JD: {0.jd}".format(s))
@@ -53,4 +39,5 @@ def sun_setting_below_angle(start_time, angle, days, stream_name):
         #print(cmd)
         subprocess.run(cmd, shell=True)
 
-sun_setting_below_angle(start_time, 20, 20, 'above20')
+for angle in (10,20,30,40,50):
+    sun_setting_below_angle(start_time, angle, 30, "above{}".format(angle))
